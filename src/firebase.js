67e241afe139495920e9e67eb627ffdfd4ab26a7
Artem Kholodenko <artemk@gmail.com>
2023-02-24 16:42:29 -0800

    additional auth functions

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 
 import {
-  GoogleAuthProvidergetAuth,
+  GoogleAuthProvider,
   getAuth,
   signInWithPopup,
   signInWithEmailAndPassword,
@@ -32,6 +32,67 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
-const analytics = getAnalytics(app)
-const db = getFirestore(app)
+export const auth = getAuth(app)
+export const analytics = getAnalytics(app)
+export const db = getFirestore(app)
+
+const googleProvider = new GoogleAuthProvider()
+
+export const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider)
+    const user = res.user
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid))
+    const docs = await getDocs(q)
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, 'users'), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'google',
+        email: user.email,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+export const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password)
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+export const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    const user = res.user
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    })
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+export const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email)
+    alert('Password reset link sent!')
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+export const logout = () => {
+  signOut(auth)
+}
