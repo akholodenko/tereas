1bcb1a454cbce34d5938fa49eab03803e1fdad87
Artem Kholodenko <artemk@gmail.com>
2023-02-25 12:41:27 -0800

    add components to sign up, in and out via firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './Components/Login'
+import Register from './Components/Register'
+import Reset from './Components/Reset'
+import Dashboard from './Components/Dashboard'
+
 function App() {
-  console.log(`api key ${process.env.REACT_APP_apiKey}`)
-  return <div className="App">test</div>
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
