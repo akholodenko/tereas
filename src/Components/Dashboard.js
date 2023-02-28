142d151abed4a4a602b18d3a788ac88732c68519
Artem Kholodenko <artemk@gmail.com>
2023-02-27 22:12:04 -0800

    focus new task list modal input field on open; use MUI vertical tabs UI for task lists

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useNavigate } from 'react-router-dom'
 import './Dashboard.css'
@@ -7,17 +7,47 @@ import { auth } from './../firebase'
 import { TaskListsContext } from '../Contexts/TaskListsContext'
 import { Grid } from '@mui/material'
 import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+
 import NewTaskListModal from './NewTaskListModal'
 
 const Dashboard = () => {
   const [user, loading] = useAuthState(auth)
   const navigate = useNavigate()
   const [taskLists, setTaskLists] = useState([])
+  const [isNewTaskModalOpen, setIsNewTaskModalOpen] = useState(false)
+
+  const [selectedTaskList, setSelectedTaskList] = React.useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setSelectedTaskList(newValue)
+  }
 
   useEffect(() => {
     if (loading) return
     if (!user) return navigate('/')
   }, [user, loading, navigate])
+
+  const onNewTaskModalOpen = () => {
+    setIsNewTaskModalOpen(true)
+  }
+
+  const onNewTaskModalClosed = () => {
+    setIsNewTaskModalOpen(false)
+    if (taskLists && taskLists.length) {
+      console.log('go', taskLists.length)
+      setSelectedTaskList(taskLists.length)
+    }
+  }
+
+  const taskListTabProps = (index: number) => {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    }
+  }
 
   return (
     <div className="dashboard">
@@ -26,13 +56,33 @@ const Dashboard = () => {
           <Grid container spacing={2}>
             <Grid item xs={4}>
               {taskLists && taskLists.length ? (
-                taskLists.map((taskList, index) => (
-                  <div key={index}>{taskList.name}</div>
-                ))
+                <Tabs
+                  orientation="vertical"
+                  variant="scrollable"
+                  value={selectedTaskList}
+                  onChange={handleChange}
+                  aria-label="Vertical tabs example"
+                  sx={{ borderRight: 1, borderColor: 'divider' }}
+                >
+                  {taskLists.map((taskList, index) => (
+                    <Tab
+                      key={index}
+                      label={taskList.name}
+                      {...taskListTabProps(index)}
+                    />
+                  ))}
+                </Tabs>
               ) : (
-                <span>no lists</span>
+                <span></span>
               )}
-              <NewTaskListModal />
+
+              <Button variant="contained" onClick={onNewTaskModalOpen}>
+                Create new list
+              </Button>
+              <NewTaskListModal
+                isOpen={isNewTaskModalOpen}
+                onNewTaskModalClosed={onNewTaskModalClosed}
+              />
             </Grid>
             <Grid item xs={8}>
               list task items
--- src/Components/NewTaskListModal.js
+++ src/Components/NewTaskListModal.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect, useRef } from 'react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Dialog from '@mui/material/Dialog'
@@ -9,52 +9,63 @@ import DialogTitle from '@mui/material/DialogTitle'
 
 import { TaskListsContext } from '../Contexts/TaskListsContext'
 
-const NewTaskListModal = () => {
-  const [open, setOpen] = useState(false)
+const ENTER = 13
+
+const NewTaskListModal = ({ isOpen, onNewTaskModalClosed }) => {
   const [taskListName, setTaskListName] = useState('')
   const { taskLists, setTaskLists } = useContext(TaskListsContext)
+  const inputRef = useRef(null)
 
-  const handleClickOpen = () => {
-    setTaskListName('')
-    setOpen(true)
-  }
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        if (inputRef.current) {
+          inputRef.current.focus()
+        }
+      }, 300)
+    }
+  }, [isOpen])
 
   const handleClose = () => {
-    setOpen(false)
+    onNewTaskModalClosed(false)
     setTaskListName('')
+  }
+
+  const handleEnterKey = (e) => {
+    if (e.keyCode === ENTER) {
+      handleCreate()
+    }
   }
 
   const handleCreate = () => {
-    console.log('create task list', taskListName)
     setTaskLists([...taskLists, { name: taskListName, tasks: [] }])
     handleClose()
   }
 
-  const handleTaskListNameChange = e => {
+  const handleTaskListNameChange = (e) => {
     setTaskListName(e.target.value)
   }
 
   return (
     <div>
-      <Button variant="contained" onClick={handleClickOpen}>
-        Create new list
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={isOpen} onClose={handleClose}>
         <DialogTitle>Create new task list</DialogTitle>
         <DialogContent>
           <DialogContentText>
             Please enter the name of the new task list.
           </DialogContentText>
           <TextField
+            inputRef={inputRef}
             autoFocus
             margin="dense"
-            id="name"
+            id="newTaskName"
             label="Task list name"
             type="text"
             fullWidth
             variant="standard"
             value={taskListName}
             onChange={handleTaskListNameChange}
+            onKeyUp={handleEnterKey}
             inputProps={{ maxLength: 25 }}
           />
         </DialogContent>
